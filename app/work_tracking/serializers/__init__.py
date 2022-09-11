e0024c4ba660684f7fd3d72b25498d9d7b889e73
osamahasanone <osamahasanone@gmail.com>
2022-09-12 00:36:22 +0200

    restructure serializers

---
+++ app/work_tracking/serializers/__init__.py
@@ -0,0 +1,12 @@
+from work_tracking.serializers.employee import (  # Noqa
+    EmployeeSerializer,
+    JobTitleSerializer,
+    TeamSerializer,
+)
+from work_tracking.serializers.tracking import (  # Noqa
+    ProjectSerializer,
+    ProjectStatsSerializer,
+    TaskSerializer,
+    TaskStateChangeSerializer,
+    WorkTimeLogSerializer,
+)
--- app/work_tracking/serializers/tracking.py
+++ app/work_tracking/serializers/tracking.py
@@ -1,48 +1,6 @@
 from rest_framework import serializers
 
-from work_tracking.models import (  # Noqa
-    Employee,
-    JobTitle,
-    Project,
-    Task,
-    TaskStateChange,
-    Team,
-    WorkTimeLog,
-)
-from work_tracking.services.team import get_team_leader
-
-
-class JobTitleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = JobTitle
-        fields = ["id", "name", "code", "created_at"]
-
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = ["id", "user", "job_title", "team", "is_leader", "created_at"]
-
-    def validate(self, data):
-        if data["is_leader"]:
-            current_team_leader = get_team_leader(data["team"])
-            if current_team_leader and (
-                not self.instance or self.instance != current_team_leader
-            ):
-                raise serializers.ValidationError(
-                    "A team should have only one leader"
-                )  # Noqa
-        return data
-
-
-class TeamSerializer(serializers.ModelSerializer):
-    employees = EmployeeSerializer(
-        source="employee_set", many=True, read_only=True
-    )  # Noqa
-
-    class Meta:
-        model = Team
-        fields = ["id", "name", "created_at", "employees"]
+from work_tracking.models import Project, Task, TaskStateChange, WorkTimeLog  # Noqa
 
 
 class ProjectSerializer(serializers.ModelSerializer):
