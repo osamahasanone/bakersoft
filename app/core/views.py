812dc0fcf42309027b769a7916d39ada4b29d53b
osamahasanone <osamahasanone@gmail.com>
2022-09-12 01:02:53 +0200

    add swagger

---
+++ app/core/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpRequest
+from django.template.response import SimpleTemplateResponse
+from rest_framework.authentication import (  # Noqa
+    BasicAuthentication,
+    SessionAuthentication,
+)
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(http_method_names=["GET"])
+@authentication_classes([BasicAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def read_docs(request: HttpRequest) -> SimpleTemplateResponse:
+    return SimpleTemplateResponse(
+        "swagger.html", {"schema_url": "openapi-schema"}
+    )  # Noqa
--- app/server/urls.py
+++ app/server/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
+from core.views import read_docs
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("openapi", get_schema_view(title=""), name="openapi-schema"),
+    path("docs", read_docs, name="docs"),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("tracking/", include("work_tracking.urls")),
